4924552ff73f29e0f64c1c625136379c8c0e6ff3
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2019-08-23 00:37:49 +0530

    Added cmd init function and parser

--- libwag.py
+++ libwag.py
@@ -1,2 +1,18 @@
-def main():
-	print ("Hello from libwag")
+import sys
+import argparse
+
+argparser = argparse.ArgumentParser(description="Another Content Tracker")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+argsubparsers.required = True
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument("path",metavar="directory", nargs="?", default=".", help="Where to create the repository.")
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    if args.command == "init":
+        cmd_init(args)
+
+
+def cmd_init(args):
+    print("Hello from init")
